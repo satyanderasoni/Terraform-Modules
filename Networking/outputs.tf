46f599574cd4c1611a633128087bfac8cb25895f
satyandera11 <satyandera11@gmail.com>
2025-10-11 12:14:51 +0530

    commit output block

--- Networking/outputs.tf
+++ Networking/outputs.tf
@@ -1,29 +1,33 @@
 output "vnets" {
   description = "Details of all created vnets"
-  value = for k, v in azurerm_virtual_network.vnet : k =>
-  {
-    name                = v.name
-    location            = v.location
-    resource_group_name = v.resource_group_name
-    address_space       = v.address_space
+  value = {
+    for k, v in azurerm_virtual_network.vnet : 
+    k => {
+      name                = v.name
+      location            = v.location
+      resource_group_name = v.resource_group_name
+      address_space       = v.address_space
+    }
   }
 }
-
 output "subnets" {
-    description = "Details of all created subnets"
-    value = for k, v in azurerm_subnet.subnet : k =>
-    {
+  description = "Details of all created subnets"
+  value = {
+    for k, v in azurerm_subnet.subnet :
+    k => {
       name                 = v.name
       resource_group_name  = v.resource_group_name
       virtual_network_name = v.virtual_network_name
       address_prefixes     = v.address_prefixes
     }
+  }
 }
 
 output "nics" {
     description = "Details of all created nics"
-    value = for k, v in azurerm_network_interface.nic : k =>
-    {
+    value = {
+    for k, v in azurerm_network_interface.nic : 
+    k => {
       name                = v.name
       location            = v.location
       resource_group_name = v.resource_group_name
@@ -33,4 +37,5 @@ output "nics" {
         private_ip_address_allocation = v.ip_configuration[0].private_ip_address_allocation
       }
     }
+  }
 }
