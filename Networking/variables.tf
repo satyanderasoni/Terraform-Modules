4ef3eed162e3d047eb8607a172553edf12dcf447
satyandera11 <satyandera11@gmail.com>
2025-10-11 20:49:35 +0530

    add vnet name

--- Networking/variables.tf
+++ Networking/variables.tf
@@ -20,6 +20,7 @@ variable "subnets" {
   description = "values for subnet"
   type = map(object({
     name             = string
+    vnet_name        = string
     address_prefixes = list(string)
   }))
 }
