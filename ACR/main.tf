f7de2762be4d25f6e37546c7606a63221faf159a
satyandera11 <satyandera11@gmail.com>
2025-10-22 21:03:14 +0530

    remove geoloactions

--- ACR/main.tf
+++ ACR/main.tf
@@ -4,14 +4,5 @@ resource "azurerm_container_registry" "acr" {
   location            = var.location
   sku                 = var.sku
   admin_enabled       = var.admin_enabled
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+
 }
