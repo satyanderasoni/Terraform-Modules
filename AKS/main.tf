b952dff36d702e7b3749def1ac0b3e5e32ff6b2b
satyandera11 <satyandera11@gmail.com>
2025-10-23 19:33:38 +0530

    commit

--- AKS/main.tf
+++ AKS/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_D2_v3"
   }
 
   identity {
