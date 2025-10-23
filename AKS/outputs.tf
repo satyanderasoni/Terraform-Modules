fd5281d3ad97e62b70ca6375ea0c486c17ebec92
satyandera11 <satyandera11@gmail.com>
2025-10-23 19:08:36 +0530

    commit

--- AKS/outputs.tf
+++ AKS/outputs.tf
@@ -1,10 +1,9 @@
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
-  sensitive = true
-}
+# output "client_certificate" {
+#   value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+#   sensitive = true
+# }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-
-  sensitive = true
-}
+# output "kube_config" {
+#   value     = azurerm_kubernetes_cluster.example.kube_config_raw
+#   sensitive = true
+# }
