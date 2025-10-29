4e39d8728536b0f58feef1919b02c1469b1d4f25
satyandera11 <satyandera11@gmail.com>
2025-10-29 22:45:09 +0530

    commit

--- AKS/main.tf
+++ AKS/main.tf
@@ -115,7 +115,7 @@ resource "azurerm_log_analytics_workspace" "law" {
 resource "azurerm_role_assignment" "acr_pull" {
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
+  scope                = var.acr_id
 }
 
 # ----------------------------
