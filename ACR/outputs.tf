bf26f105c5373987ee445f173307f88ee8177710
satyandera11 <satyandera11@gmail.com>
2025-10-29 22:33:44 +0530

    commit

--- ACR/outputs.tf
+++ ACR/outputs.tf
@@ -7,7 +7,6 @@ output "acr_id" {
   description = "The ID of the Azure Container Registry used for AKS image pulls"
   value       = azurerm_container_registry.acr.id
 }
-
 output "acr_name" {
   value = azurerm_container_registry.acr.name
 }
